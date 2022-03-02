4292a8017c51b3235f3a978b3ae50bfecb574085
Rak Mi <someonex666@gmail.com>
2022-03-02 15:11:54 -0500

    Test screeshot in app

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-from screen_scrape import scrape
+from screen_scrape import scrape, screenshot
 
-scrape.get_aution_house_prices("market_data/screenshot.png")
+
+window = 'LOST ARK (64-bit, DX11) v.2.0.2.1'
+screenshot_path = screenshot.take_screenshot_of_window(window)
+
+descriptor = 'enhancement_page_4'
+scrape.get_aution_house_prices(screenshot_path,descriptor)
+
