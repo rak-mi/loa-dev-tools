3f4c37a7c8215e0b6561f5eb4e5807a6627c297e
Rak Mi <someonex666@gmail.com>
2022-03-03 17:11:33 -0500

    Modify app.py
    Modify app to use screenshot folders instead of key on date-time

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
-from screen_scrape import scrape, screenshot
+from locale import currency
+from pprint import PrettyPrinter
+from screen_scrape import scrape, auto_utils
+from db_handle import db
+import json
+import configparser
+import datetime
+import glob
 
+#Open up config file and get the db_url
+config = configparser.ConfigParser()
+config.read('conf.ini')
+db_url = config['mongo-db']['url']
 
-window = 'LOST ARK (64-bit, DX11) v.2.0.2.1'
-screenshot_path = screenshot.take_screenshot_of_window(window)
+#list of screenshots to process
 
-descriptor = 'enhancement_page_4'
-scrape.get_aution_house_prices(screenshot_path,descriptor)
+mari_shop_list = ['mari_shop_honing', 'mari_shop_tradeskill']
 
+enhancement_file_list = glob.glob("screenshots\\market\\*.png")
+
+#process each market screenshot and upload to db
+index = 0
+for enhancement_file in enhancement_file_list:
+    enhancement = (enhancement_file[-36:-18])
+    date_time = (enhancement_file[-17:-4])
+    
+    response = scrape.get_aution_house_prices(enhancement_file, enhancement, date_time)
+    db.upload_market_to_db(response, db_url, date_time)
+    index += 1
+
+mari_file_list = glob.glob("screenshots\\mari\\*.png")
+#process each mari screenshot and upload to db
+for mari_shop_file in mari_file_list:
+
+    date_time = (mari_shop_file[-17:-4])
+    mari_shop = (mari_shop_file[-26:-18])
+    mari_slot = auto_utils.convert_time_into_mari_time(date_time)
+    response = scrape.get_mari_prices(mari_shop_file, mari_shop, mari_slot)
+    db.upload_mari_to_db(response, db_url, mari_slot)
+
+#process currency exchange and upload to db
+currency_file_list = glob.glob("screenshots\\currency\\*.png")
+for currency_file in currency_file_list:
+    date_time = (currency_file[-17:-4])
+    avg_currency = int(scrape.get_currency_exchange(currency_file, 'currency_exchange'))
+    db.upload_currency_to_db(avg_currency, db_url, date_time)
