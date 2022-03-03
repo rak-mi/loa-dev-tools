997352e54f5e3a632914a986fef65646d2c1e448
Rak Mi <someonex666@gmail.com>
2022-03-03 17:09:35 -0500

    Function to determine mari time slot

--- screen_scrape/auto_utils.py
+++ screen_scrape/auto_utils.py
@@ -1,27 +1,15 @@
-import win32gui
-from pynput.mouse import Button, Controller
+def convert_time_into_mari_time(time):
+    time_split = time.split('-')
+    date = time_split[0]
+    time = int(time_split[1])
 
-def move_mouse():
-
-    hwnd = win32gui.FindWindowEx(0,0,0, "LOST ARK (64-bit, DX11) v.2.0.2.1")
-    win32gui.SetForegroundWindow(hwnd)
-
-    mouse = Controller()
-
-    # Read pointer position
-    print('The current pointer position is {0}'.format(
-        mouse.position))
-
-    # Move pointer relative to current position
-    mouse.move(5, -5)
-
-    # Press and release
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-
-    # Double click; this is different from pressing and releasing
-    # twice on Mac OSX
-    mouse.click(Button.left, 2)
-
-    # Scroll two steps down
-    mouse.scroll(0, 2)
+    if time >= 1 and time < 7:
+        return date + '-0100-0700'
+    elif time >= 7 and time < 13:
+        return date + '-0700-1300'
+    elif time >= 13 and time < 19:
+        return date + '-1300-1900'
+    elif time >= 19 or time == 0:
+        return date + '-1900-0100'
+    else:
+        return date + '-0000-0000'
