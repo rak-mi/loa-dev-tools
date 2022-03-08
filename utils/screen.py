a7396cd0eb1fc5f669eb99e99596520dd2bb778b
Rak Mi <someonex666@gmail.com>
2022-03-08 15:57:48 -0500

    use -H instead of -h

--- utils/screen.py
+++ utils/screen.py
@@ -7,8 +7,8 @@ parser = argparse.ArgumentParser(description='nah')
 parser.add_argument('-type1', metavar='t', type=str,help='Screenshot folder')
 parser.add_argument('-type2', metavar='p', type=str, help='Market or Currency description')
 parser.add_argument('-mode', metavar='m', type=str, help='Mode (Screenshot description')
-parser.add_argument('-w', metavar='m', type=str, help='Width')
-parser.add_argument('-h', metavar='m', type=str, help='Height')
+parser.add_argument('-W', metavar='m', type=str, help='Width')
+parser.add_argument('-H', metavar='m', type=str, help='Height')
 
 args = parser.parse_args()
 
