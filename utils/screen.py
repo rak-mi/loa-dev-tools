18d48c645d11498e4dd28c4f7cf04728fedd29c8
Rak Mi <someonex666@gmail.com>
2022-03-08 16:10:24 -0500

    Update with better args

--- utils/screen.py
+++ utils/screen.py
@@ -1,15 +1,15 @@
-from utils import screenshot
+import screenshot
 
 import argparse
 
 parser = argparse.ArgumentParser(description='nah')
 
-parser.add_argument('-type1', metavar='t', type=str,help='Screenshot folder')
-parser.add_argument('-type2', metavar='p', type=str, help='Market or Currency description')
-parser.add_argument('-mode', metavar='m', type=str, help='Mode (Screenshot description')
-parser.add_argument('-W', metavar='m', type=str, help='Width')
-parser.add_argument('-H', metavar='m', type=str, help='Height')
+parser.add_argument('-t', metavar='type1', type=str,help='Screenshot folder')
+parser.add_argument('-p', metavar='type2', type=str, help='Market or Currency description')
+parser.add_argument('-m', metavar='mode', type=str, help='Mode (Screenshot description')
+parser.add_argument('-W', metavar='w', type=str, help='Width')
+parser.add_argument('-H', metavar='h', type=str, help='Height')
 
 args = parser.parse_args()
 
-screenshot.take_screenshot_of_window(args.type1, args.type2, args.mode, args.w, args.h)
+screenshot.take_screenshot_of_window(args.t, args.p, args.m, args.W, args.H)
