aee2455f7285af85faa730662d798f4d1c94171f
Rak Mi <someonex666@gmail.com>
2022-03-07 22:38:00 -0500

    Remove Mari code
    Remove code to analyze mari market, and added different modes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from locale import currency
 from pprint import PrettyPrinter
 from screen_scrape import scrape, auto_utils
-from db_handle import db, analysis
+from db_handle import analysis, mongdb
 import json
 import configparser
 import datetime
@@ -14,57 +14,46 @@ config = configparser.ConfigParser()
 config.read('conf.ini')
 db_url = config['mongo-db']['url']
 
-parser = argparse.ArgumentParser(description='nah')
-
-parser.add_argument('width', metavar='w', type=str,
-                    help='nah')
-
-parser.add_argument('height', metavar='h', type=str, 
-                    help='nah')
+#Arguments to choose mode/width/height
+parser = argparse.ArgumentParser(description='args')
+parser.add_argument('-mode', '-M', metavar='mode',  type=str, help='market mode or currency exchange mode')
+parser.add_argument('-width', '-W',  metavar='width', type=str, help='width of screen')
+parser.add_argument('-height','-H',  metavar='height',type=str, help='hieght of screen')
 args = parser.parse_args()
+
 #list of screenshots to process
-
-mari_shop_list = ['mari_shop_honing', 'mari_shop_tradeskill']
-
 enhancement_file_list = glob.glob("screenshots\\market\\*.png")
 
-#process each market screenshot and upload to db
-index = 0
-for enhancement_file in enhancement_file_list:
-    try:
-        enhancement = (enhancement_file[-36:-18])
-        date_time = (enhancement_file[-17:-4])
-        
-        response = scrape.get_aution_house_prices(enhancement_file, enhancement, date_time, args.width, args.height)
-        db.upload_market_to_db(response, db_url, date_time)
-        index += 1
-    except Exception as e:
-        print("Error processing screenshot: ", enhancement_file)
-        print(e)
-        continue
+#get information from market
+if args.mode == 'market':
+    #process each market screenshot and upload to db
+    for enhancement_file in enhancement_file_list:
+        try:
+            enhancement = (enhancement_file[-36:-18])
+            date_time = (enhancement_file[-17:-4])   
+            response = scrape.get_aution_house_prices(enhancement_file, enhancement, date_time, args.width, args.height)
+            mongdb.upload_market_to_db(response, db_url, date_time)
 
-mari_file_list = glob.glob("screenshots\\mari\\*.png")
-""" #process each mari screenshot and upload to db
-for mari_shop_file in mari_file_list:
-    try:
-        date_time = (mari_shop_file[-17:-4])
-        mari_shop = (mari_shop_file[-26:-18])
-        mari_slot = auto_utils.convert_time_into_mari_time(date_time)
-        response = scrape.get_mari_prices(mari_shop_file, mari_shop, mari_slot)
-        db.upload_mari_to_db(response, db_url, mari_slot)
-    except Exception as e:
-        print("Error processing screenshot: ", mari_shop_file)
-        print(e)
-        continue
+        except Exception as e:
+            print("Error processing screenshot: ", enhancement_file)
+            print(e)
+            continue
 
-#process currency exchange and upload to db
-currency_file_list = glob.glob("screenshots\\currency\\*.png")
-for currency_file in currency_file_list:
-    try:
-        date_time = (currency_file[-17:-4])
-        avg_currency = int(scrape.get_currency_exchange(currency_file, 'currency_exchange'))
-        db.upload_currency_to_db(avg_currency, db_url, date_time)
-    except Exception as e:
-        print("Error processing screenshot: ", currency_file)
-        print(e)
-        continue """
+#get information from currency exchange
+elif args.mode == 'currency':
+    #process currency exchange and upload to db
+    currency_file_list = glob.glob("screenshots\\currency\\*.png")
+    for currency_file in currency_file_list:
+        try:
+            date_time = (currency_file[-17:-4])
+            avg_currency = int(scrape.get_currency_exchange(currency_file, 'currency_exchange'))
+            mongdb.upload_currency_to_db(avg_currency, db_url, date_time)
+        except Exception as e:
+            print("Error processing screenshot: ", currency_file)
+            print(e)
+            continue
+
+#unknown mode
+else:
+    print("Invalid mode")
+    exit()
