f5a01553defc860672b4d517c0cfdcc9c196ea25
Rak Mi <someonex666@gmail.com>
2022-03-07 23:13:26 -0500

    Move sheets to util folder
    Change some args for utils

--- utils.py/screen.py
+++ utils.py/screen.py
@@ -4,20 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='nah')
 
-parser.add_argument('type1', metavar='t', type=str,
-                    help='nah')
-
-parser.add_argument('type2', metavar='p', type=str, 
-                    help='nah')
-
-parser.add_argument('mode', metavar='m', type=str, 
-                    help='nah')
-
-parser.add_argument('w', metavar='m', type=str, 
-                    help='nah')
-
-parser.add_argument('h', metavar='m', type=str, 
-                    help='nah')
+parser.add_argument('-type1', metavar='t', type=str,help='Screenshot folder')
+parser.add_argument('-type2', metavar='p', type=str, help='Market or Currency description')
+parser.add_argument('-mode', metavar='m', type=str, help='Mode (Screenshot description')
+parser.add_argument('-w', metavar='m', type=str, help='Width')
+parser.add_argument('-h', metavar='m', type=str, help='Height')
 
 args = parser.parse_args()
 
